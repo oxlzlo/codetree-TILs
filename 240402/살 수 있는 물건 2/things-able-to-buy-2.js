ee2fd70550d5943d71056f55b82a7fc9fb027fde
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-02 02:13:15 +0900

    Title: 살 수 있는 물건 2, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240402/살 수 있는 물건 2/things-able-to-buy-2.js
+++ 240402/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -7,7 +7,7 @@ if (n >= 3000) {
     console.log('book');
 } else if (n >= 1000) {
     console.log('mask');
-} else if (n >= pen) {
+} else if (n >= 500) {
     console.log('pen');
 } else {
     console.log('no')
