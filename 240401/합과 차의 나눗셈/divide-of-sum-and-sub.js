bbaaeb83874c7db395abc592f8c2e44249a29b76
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-01 22:44:04 +0900

    Title: 합과 차의 나눗셈, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 240401/합과 차의 나눗셈/divide-of-sum-and-sub.js
+++ 240401/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split(' ');
 
-const a = input[0];
-const b = input[1];
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
 
-console.log(Math.ceil((a+b)/(a-b)).toFixed(2))
+console.log(((a+b)/(a-b)).toFixed(2))
