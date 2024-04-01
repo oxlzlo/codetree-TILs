fb6b5abf5b42467e62c5ead337d7ea7d9b17c30a
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-02 02:10:45 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString();
+
+const n = parseInt(input);
+
+if (n >= 3000) {
+    console.log('book');
+} else if (n >= 1000) {
+    console.log('mask');
+} else if (n >= pen) {
+    console.log('pen');
+} else {
+    console.log('no')
+}
