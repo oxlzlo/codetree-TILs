9d0faace40bdda5973430747532f2520221b94e0
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-03 23:17:30 +0900

    Title: 4번의 크기 비교, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240403/4번의 크기 비교/4-time-comparison.js
+++ 240403/4번의 크기 비교/4-time-comparison.js
@@ -4,26 +4,7 @@ const input = fs.readFileSync(0).toString().split('\n');
 const a = input.shift()
 const [b, c, d, e] = input.toString().split(' ');
 
-if (a > b) {
-    console.log(1)
-} else {
-    console.log(0)
-}
-
-if (a > c) {
-    console.log(1)
-} else {
-    console.log(0)
-}
-
-if (a > d) {
-    console.log(1)
-} else {
-    console.log(0)
-}
-
-if (a > e) {
-    console.log(1)
-} else {
-    console.log(0)
-}
+console.log(a > b ? 1 : 0);
+console.log(a > c ? 1 : 0);
+console.log(a > d ? 1 : 0);
+console.log(a > e ? 1 : 0);
