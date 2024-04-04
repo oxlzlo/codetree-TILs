384be64d51ec8ef0e3e380155989292374cdb448
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-04 18:24:51 +0900

    Title: 남녀노소 구분짓기, Time: 266ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240404/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+
+const gender = Number(input[0]);
+const age = Number(input[1]);
+
+if (gender == 0) {
+    if (age >= 19) {
+        console.log('MAN')
+    } else {
+        console.log('BOY')
+    }
+}
+
+if (gender == 1) {
+    if (age >= 19) {
+        console.log('WOMAN')
+    } else {
+        console.log('GIRL')
+    }
+}
