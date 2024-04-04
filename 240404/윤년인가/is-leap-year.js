6e888e3bf62072ab23a83c8a38525aee12b834c6
김여진 <140046183+oxlzlo@users.noreply.github.com>
2024-04-04 18:52:55 +0900

    Title: 윤년인가, Time: 305ms, Memory: 14MB, Status: Passed - CodeTree

--- 240404/윤년인가/is-leap-year.js
+++ 240404/윤년인가/is-leap-year.js
@@ -4,13 +4,10 @@ const input = fs.readFileSync(0).toString();
 const y = parseInt(input);
 
 if (y%4 === 0) {
-    if (y%100 === 0) {
-        console.log(false)
-    }
     if (y%100 === 0 && y%400 !== 0) {
-        console.log(true)
+        return console.log(false)
     }
-    console.log(true)
+    return console.log(true)
 } else {
-    console.log(false)
+    return console.log(false)
 }
